e5d8e32b7567fa7539a309d2624e0ac7fc1d7195
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-27 23:30:00 +0800

    Adding mongoDB connection

--- models/score.js
+++ models/score.js
@@ -1,4 +1,20 @@
 const mongoose = require("mongoose");
+const logger = require("../utils/logger");
+
+const url = process.env.MONGODB_URI
+
+logger.info('connecting to mongoDB');
+
+mongoose.connect(url, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useFindAndModify: false, 
+  useCreateIndex: true 
+}).then(res => {
+  logger.info('connected to mongoDB')
+}).catch(err => {
+  logger.error('error connecting to mongoDB: ', err.message)
+})
 
 const scoreSchema = new mongoose.Schema({
   score: Number,
