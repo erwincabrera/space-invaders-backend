7d29c0d94c9d8ae0c84831516e8c6062e8d53368
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-28 16:33:15 +0800

    Adding files from the frontend

---
+++ build/precache-manifest.9db023f404ea2a5c9e7efe173e9957be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d0694e7c0ea1ada798da5269d43eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ddeb32abcc02c062f5c",
+    "url": "/static/css/main.65889475.chunk.css"
+  },
+  {
+    "revision": "92c5fc46983b951d57e3",
+    "url": "/static/js/2.ff4d210a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff4d210a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddeb32abcc02c062f5c",
+    "url": "/static/js/main.1b812e94.chunk.js"
+  },
+  {
+    "revision": "a597d3f20da3abfbe6bb",
+    "url": "/static/js/runtime-main.6362b0eb.js"
+  },
+  {
+    "revision": "ca1a827ebd4c6051206da57994886d28",
+    "url": "/static/media/bg-game.ca1a827e.jpg"
+  },
+  {
+    "revision": "d2bbf04326a5eb864920e04e38b29ed2",
+    "url": "/static/media/bg-start.d2bbf043.jpg"
+  },
+  {
+    "revision": "b68af7471090dd84a20e3a9dc3708239",
+    "url": "/static/media/invader-death.b68af747.mp3"
+  },
+  {
+    "revision": "9a96bd661d16b9fb19032a647cc58ec3",
+    "url": "/static/media/photon-torpedos.9a96bd66.mp3"
+  }
+]);
