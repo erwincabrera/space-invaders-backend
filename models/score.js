df980c7b711e52c1634f0d594ee9448e1b664255
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-29 12:56:08 +0800

    Adding user information to the score schema

--- models/score.js
+++ models/score.js
@@ -25,6 +25,10 @@ const scoreSchema = new mongoose.Schema({
     type: Date,
     required: true
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
 scoreSchema.set('toJSON', {
