8f60eab52bd54737f3a176c909d2cfb410e8ba65
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-29 14:37:00 +0800

    Adding handler for GET "users/" route

--- controllers/users.js
+++ controllers/users.js
@@ -2,91 +2,10 @@ const usersRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
-const users = [
-  {
-    username: "user1",
-    score: 1,
-  },
-  {
-    username: "user2",
-    score: 2,
-  },
-  {
-    username: "user3",
-    score: 3,
-  },
-  {
-    username: "user4",
-    score: 4,
-  },
-  {
-    username: "user5",
-    score: 5,
-  },
-  {
-    username: "user6",
-    score: 1,
-  },
-  {
-    username: "user7",
-    score: 2,
-  },
-  {
-    username: "user8",
-    score: 3,
-  },
-  {
-    username: "user9",
-    score: 4,
-  },
-  {
-    username: "user10",
-    score: 5,
-  },
-  {
-    username: "user11",
-    score: 1,
-  },
-  {
-    username: "user12",
-    score: 2,
-  },
-  {
-    username: "user13",
-    score: 3,
-  },
-  {
-    username: "user14",
-    score: 4,
-  },
-  {
-    username: "user15",
-    score: 5,
-  },
-  {
-    username: "user16",
-    score: 1,
-  },
-  {
-    username: "user17",
-    score: 2,
-  },
-  {
-    username: "user18",
-    score: 3,
-  },
-  {
-    username: "user19",
-    score: 4,
-  },
-  {
-    username: "user20",
-    score: 5,
-  },
-];
-
-usersRouter.get("/", (req, res) => {
-  res.json(users);
+usersRouter.get("/", (req, res, next) => {
+  User.find({})
+    .then(users => res.json(users))
+    .catch(next)
 });
 
 usersRouter.post("/", (req, res, next) => {
