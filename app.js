f36eb5c831a5066db8cf41fd488ce709909456d3
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-30 16:54:33 +0800

    Adding initial login functionality

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const scoresRouter = require("./controllers/scores");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/scores', scoresRouter);
 
---
+++ controllers/login.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
+
+loginRouter.post("/", async (req, res, next) => {
+  try {
+    const body = req.body;
+    const user = await User.findOne({ username: body.username });
+    const isCorrectPassword = user == undefined 
+      ? false 
+      : await bcrypt.compare(body.password, user.passwordHash);
+
+    if (!isCorrectPassword) {
+      return res.status(401).json({
+        error: "Invalid username or password."
+      });
+    }
+
+    const token = jwt.sign({
+      username: user.username,
+      id: user._id
+    }, process.env.SECRET);
+
+    res.status(200).send({
+      token,
+      username: user.username,
+      name: user.name
+    });
+
+  } catch(err) {
+    next(err);
+  }
+});
+
+module.exports = loginRouter;
