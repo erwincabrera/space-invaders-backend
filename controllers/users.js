8da96497a99906336a492f4c93b22fdd8878b0c2
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-11-30 16:13:23 +0800

    Populating scores in GET "users/" route

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,17 @@ const usersRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
-usersRouter.get("/", (req, res, next) => {
-  User.find({})
-    .then(users => res.json(users))
-    .catch(next)
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User
+      .find({})
+      .populate('scores', { score: 1, date: 1 });
+
+    res.json(users);
+
+  } catch (err) {
+    next(err);
+  }
 });
 
 usersRouter.post("/", (req, res, next) => {
