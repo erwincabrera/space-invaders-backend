f8c76ba1646fe798dd8c18ae6b82a7a09e0b716f
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2022-10-20 08:27:38 +0800

    Move mongoDB connection to app.js

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const scoresRouter = require("./controllers/scores");
 const loginRouter = require("./controllers/login");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+
+const url = process.env.MONGODB_URI;
+
+logger.info("connecting to mongoDB");
+
+mongoose
+  .connect(url)
+  .then((_res) => {
+    logger.info("connected to mongoDB");
+  })
+  .catch((err) => {
+    logger.error("error connecting to mongoDB: ", err.message);
+  });
 
 const app = express();
 
--- models/score.js
+++ models/score.js
@@ -1,18 +1,4 @@
 const mongoose = require("mongoose");
-const logger = require("../utils/logger");
-
-const url = process.env.MONGODB_URI;
-
-logger.info("connecting to mongoDB");
-
-mongoose
-  .connect(url)
-  .then((_res) => {
-    logger.info("connected to mongoDB");
-  })
-  .catch((err) => {
-    logger.error("error connecting to mongoDB: ", err.message);
-  });
 
 const scoreSchema = new mongoose.Schema({
   score: {
