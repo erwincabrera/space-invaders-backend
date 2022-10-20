471dfc187b5009b0bf25ad9f24903f79242b5e18
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2022-10-20 08:33:23 +0800

    Restructure project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const app = require("./app");
-const logger = require("./utils/logger");
+const app = require("./src/app");
+const logger = require("./src/utils/logger");
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
