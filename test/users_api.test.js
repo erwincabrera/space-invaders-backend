46741645e5f6d9a1303f535863b5b5d4a65e9004
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2022-10-31 17:17:15 +0800

    Add Users API test

--- test/users_api.test.js
+++ test/users_api.test.js
@@ -1,3 +1,107 @@
-test("no tests yet", () => {
-  return;
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../src/app");
+const User = require("../src/models/user");
+
+const api = supertest(app);
+
+const BASE_URL = "/api/users";
+
+describe("users API", () => {
+  describe("create", () => {
+    test("response contains correct fields", async () => {
+      const testUser = {
+        username: "test",
+        password: "asdf123",
+      };
+
+      const { body } = await api
+        .post(BASE_URL)
+        .send(testUser)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+      expect(body).toHaveProperty("id");
+      expect(body.username).toEqual(testUser.username);
+      expect(Array.isArray(body.scores)).toBe(true);
+      expect(body.scores.length).toBe(0);
+    });
+
+    test("request without password is rejected", async () => {
+      const testUser = {
+        username: "test",
+      };
+
+      const expected = (await User.find({})).length;
+      await api.post(BASE_URL).send(testUser).expect(400);
+      const actual = (await User.find({})).length;
+      expect(actual).toEqual(expected);
+    });
+
+    test("request without username is rejected", async () => {
+      const testUser = {
+        password: "test",
+      };
+
+      const expected = (await User.find({})).length;
+      await api.post(BASE_URL).send(testUser).expect(400);
+      const actual = (await User.find({})).length;
+      expect(actual).toEqual(expected);
+    });
+
+    afterAll(async () => {
+      await User.deleteMany({});
+    });
+  });
+
+  describe("read", () => {
+    const testUsers = [
+      {
+        username: "foo",
+        password: "foo",
+      },
+      {
+        username: "bar",
+        password: "bar",
+      },
+    ];
+
+    beforeAll(async () => {
+      for (let i = 0; i < testUsers.length; i++) {
+        const { body } = await api
+          .post(BASE_URL)
+          .send(testUsers[i])
+          .expect(201);
+        testUsers[i].id = body.id;
+      }
+    });
+
+    test("returns all users as an array with correct fields", async () => {
+      const { body } = await api
+        .get(BASE_URL)
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
+      expect(Array.isArray(body)).toBe(true);
+      expect(body.length).toEqual(testUsers.length);
+
+      for (let i = 0; i < testUsers.length; i++) {
+        const testUser = testUsers[i];
+        const actual = body.find((b) => b.username === testUser.username);
+
+        expect(actual).toBeDefined();
+        expect(actual.id).toEqual(testUser.id);
+        expect(actual.username).toEqual(testUser.username);
+        expect(Array.isArray(actual.scores)).toBe(true);
+      }
+    });
+
+    afterAll(async () => {
+      await User.deleteMany({});
+    });
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 });
